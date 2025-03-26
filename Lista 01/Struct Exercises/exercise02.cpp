21e0a9c4b32b554ab2df5a9e8eff16f0ec05b42b
Bruno <bomf@cesar.school>
2025-03-26 13:36:36 -0300

    add exercise 02 solution code

--- Lista 01/Struct Exercises/exercise02.cpp
+++ Lista 01/Struct Exercises/exercise02.cpp
@@ -13,10 +13,42 @@ using namespace std;
 typedef struct Aluno {
     char nome[50];
     int idade;
-    float altura;
+    float nota;
 } Aluno;
 
+void preencheAluno(Aluno arrayAlunos[]) {
+    for (int i = 0; i < 5; i++) {
+        cout << "Informe o nome do aluno " << i + 1 << ": ";
+        cin.ignore();
+        cin.getline(arrayAlunos[i].nome, 50);
+        cout << "A idade do aluno " << i + 1 << ": ";
+        cin >> arrayAlunos[i].idade;
+        cout << "E a nota do aluno " << i << ": ";
+        cin >> arrayAlunos[i].nota;
+        cout << endl;
+    }
+}
+
+float calculaMedia(Aluno arrayAlunos[]) {
+    float somaNotas;
+    float media;
+
+    for (int i = 0; i < 5; i++) {
+        somaNotas = somaNotas + arrayAlunos[i].nota;
+    }
+    return media = somaNotas / 5;
+}
+
+void exibeMedia(float media) {
+    cout << "A media dos 5 alunos eh de: " << media << endl;
+}
+
 int main() {
+    Aluno arrayAlunos[4];
+
+    preencheAluno(arrayAlunos);
+    float media = calculaMedia(arrayAlunos);
+    exibeMedia(media);
 
     return 0;
 }
