475b6f3a4d5a7b7af2034c78e7087eb8def496f1
Bruno <bomf@cesar.school>
2025-03-28 16:46:36 -0300

    add exercise 7

--- List 01/Struct Exercises/exercise07.cpp
+++ List 01/Struct Exercises/exercise07.cpp
@@ -5,15 +5,61 @@
 
 #include <stdio.h>
 #include <iostream>
+#include <string.h>
 
 using namespace std;
 
 typedef struct Contato {
     char nome[50];
-    char telefone[50];
+    char telefone[15];
 } Contato;
 
+Contato* adicionaContatos(int qtdContatos) {
+    Contato* contatos = (Contato*)malloc(qtdContatos * sizeof(Contato));
+    
+    for (int i = 0; i < qtdContatos; i++) {
+        cout << "Nome do contato " << i + 1 << ": " << endl;
+        cin >> contatos[i].nome;
+        cout << "Telefone do contato " << i + 1 << ": " << endl;
+        cin >> contatos[i].telefone;
+    }
+    cout << "---- Contatos adicionados com sucesso! ----" << endl;
+    
+    return contatos;
+}
+
+void buscaContatos(Contato* contatos, char* nome, int qtdContatos) {
+    for (int i = 0; i < qtdContatos; i++) {
+        if (strcmp(contatos[i].nome, nome) == 0) {
+            cout << "Os dados do contato buscado sao:" << endl;
+            cout << "-----------------" << endl;
+            cout << "Nome: " << contatos[i].nome << endl;;
+            cout << "Telefone: " << contatos[i].telefone << endl;
+            cout << "-----------------" << endl;
+        }
+    }
+}
+
 int main() {
+    Contato* contatos;
+    char nomeContato[50];
+    int qtdContatos;
+
+    cout << "-- Agenda Telefonica --" << endl;
+    cout << "Adicione seu contatos." << endl;
+    cout << "Quantos contatos deseja cadastrar?\n-> ";
+    cin >> qtdContatos;
+    
+    contatos = adicionaContatos(qtdContatos);
+
+    cout << "- Sistema de Busca de Contatos -" << endl;
+    cout << "Busque um contato pelo nome" << endl;
+    cout << "Digite o nome do contato a ser buscado: ";
+    cin >> nomeContato;
+
+    buscaContatos(contatos, nomeContato, qtdContatos);
+
+    free(contatos);
 
     return 0;
 }
