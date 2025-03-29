6c6da8210f64a2f3792ddc7d853770feb19bfa61
Bruno <bomf@cesar.school>
2025-03-29 08:14:21 -0300

    add exercises 8 and 9

--- List 01/Struct Exercises/exercise08.cpp
+++ List 01/Struct Exercises/exercise08.cpp
@@ -1,11 +1,49 @@
+// 8. Cálculo de Área e Perímetro
+// Crie uma struct chamada Retangulo, que contém:
+// • Base (float)
+// • Altura (float)
+// Crie funções para calcular a área e o perímetro do retângulo. No main(), permita
+// que o usuário insira os valores e exiba os cálculos.
 #include <stdio.h>
 #include <iostream>
 
 using namespace std;
 
+typedef struct Retangulo {
+    float base;
+    float altura;
+} Retangulo;
 
+float calculaArea(float base, float altura) {
+    float area;
+    area = base * altura;
+    
+    return area;
+}
+
+float calculaPerimetro(float base, float altura) {
+    float perimetro;
+    perimetro = 2 * (base + altura);
+
+    return perimetro;
+}
 
 int main() {
+    Retangulo* retangulo1 = (Retangulo*)malloc(sizeof(Retangulo));
+    float area;
+    float perimetro;
+
+    cout << "-- Calculo de Area e Perimetro de um Retangulo --" << endl;
+    cout << "Informe a base: ";
+    cin >> retangulo1->base;
+    cout << "Informe a altura: ";
+    cin >> retangulo1->altura;
+
+    area = calculaArea(retangulo1->base, retangulo1->altura);
+    perimetro = calculaPerimetro(retangulo1->base, retangulo1->altura);
+
+    cout << endl << "A area deste triangulo eh: " << area << "." << endl;
+    cout << "E o perimetro dele eh: " << perimetro << "." << endl;
 
     return 0;
 }
--- List 01/Struct Exercises/exercise09.cpp
+++ List 01/Struct Exercises/exercise09.cpp
@@ -1,11 +1,56 @@
+// 9. Registro de Pacientes
+// Crie uma struct chamada Paciente, contendo:
+// • Nome (string de até 50 caracteres)
+// • Idade (inteiro)
+// • Diagnóstico (string de até 100 caracteres)
+// Crie uma função que recebe um vetor de Paciente e exibe todos os pacientes
+// acima de 60 anos.
 #include <stdio.h>
 #include <iostream>
 
 using namespace std;
 
+typedef struct Paciente {
+    char nome[50];
+    int idade;
+    char diagnostico[100];
+}Paciente;
 
+void exibePacienteIdade(Paciente* pacientes) {
+    int idadePaciente;
+    cout << "Voce deseja buscar um paciente a partir de qual idade?\n-> ";
+    cin >> idadePaciente;
+
+    cout << endl << "Estes sao os pacientes com idade maior ou igual a " << idadePaciente << "." << endl;
+    cout << "-----------------------------" << endl;
+    for (int i = 0; pacientes[i].idade >= idadePaciente; i++) {
+        cout << "Nome do paciente: " << pacientes[i].nome << endl;
+        cout << "Idade do paciente: " << pacientes[i].idade << endl;
+        cout << "Diagnostico do paciente: " << pacientes[i].diagnostico << endl;
+        cout << "-----------------------------" << endl;
+    }
+
+}
 
 int main() {
+    Paciente pacientes[3];
+
+    cout << "-- Cadastro de Pacientes --" << endl;
+    cout << "Informe nome, idade e o diagnostico deles." << endl;
+    
+    for (int i = 0; i < 3; i++) {
+        cout << "Nome do paciente " << i + 1 << ": ";
+        cin.getline(pacientes[i].nome, 50);
+        cout << "Idade do paciente " << i + 1 << ": ";
+        cin >> pacientes[i].idade;
+        cin.ignore(); // limpando o \n deixado no buffer dps de ler idade
+        cout << "Diagnostico do paciente " << i + 1 << ": ";
+        cin.getline(pacientes[i].diagnostico, 100);
+        cout << "-----------------------------" << endl;
+    }
+    cout << "-- Cadastro realizado com sucesso! -- " << endl;
+    exibePacienteIdade(pacientes);
+
 
     return 0;
 }
