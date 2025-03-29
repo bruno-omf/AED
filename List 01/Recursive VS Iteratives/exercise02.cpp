f3567b5fb48586cb2c38be4e0d9a3e9e95df373f
Bruno <bomf@cesar.school>
2025-03-29 15:33:20 -0300

    add code structure to the recursives/iteratives exercises

---
+++ List 01/Recursive VS Iteratives/exercise02.cpp
@@ -0,0 +1,17 @@
+// 2. Maior Elemento em um Vetor
+// Crie uma função recursiva que recebe um vetor de inteiros e seu tamanho n e
+// retorna o maior valor presente no vetor.
+// Exemplo:
+// int maior_elemento(int vet[], int n);
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+
+
+int main() {
+
+    return 0;
+}
