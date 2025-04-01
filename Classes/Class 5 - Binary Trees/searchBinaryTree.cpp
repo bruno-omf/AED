7d022fb2846dd737f6ff46c0fc4e2854a989854f
Bruno <bomf@cesar.school>
2025-04-01 09:14:12 -0300

    add class 5 BT exercises

--- Classes/Class 5 - Binary Trees/searchBinaryTree.cpp
+++ Classes/Class 5 - Binary Trees/searchBinaryTree.cpp
@@ -11,7 +11,7 @@ typedef struct Node {
 
 Node* createNode(int data) {
     Node* newNode = (Node*)malloc(sizeof(Node));
-    if (newNode == NULL;) {
+    if (newNode == NULL) {
         cout << "Memory allocation error!" << endl;
         exit(1); // termina o programa completamente
     } else {
@@ -23,15 +23,20 @@ Node* createNode(int data) {
 }
 
 Node* insertNode(Node* root, int data) {
-    Node* newNode = createNode(data);
     if (root == NULL) {
-        return newNode;
+        return createNode(data);
     }
+    if (data < root->data) {
+        root->left = insertNode(root->left, data);
+    } else if (data > root->data) {
+        root->right = insertNode(root->right, data);
+    }
+    return root;
 }
 
 void preOder(Node* root) {
     if (root != NULL) {
-        cout << root->data;
+        cout << root->data << " ";
         preOder(root->left);
         preOder(root->right);
     }
@@ -40,7 +45,7 @@ void preOder(Node* root) {
 void inOrder(Node* root) {
     if (root != NULL) {
         inOrder(root->left);
-        cout << root->data;
+        cout << root->data << " ";
         inOrder(root->right);
     }
 }
@@ -49,11 +54,32 @@ void postOrder(Node* root) {
     if (root != NULL) {
         postOrder(root->left);
         postOrder(root->right);
-        cout << root->data;
+        cout << root->data << " ";
     }
 }
 
 int main() {
+    Node * root = NULL; // criar um no raiz inicializando com null
+    
+    root = insertNode(root, 50);
+    insertNode(root, 30);
+    insertNode(root, 70);
+    insertNode(root, 20);
+    insertNode(root, 40);
+    insertNode(root, 60);
+    insertNode(root, 80);
+
+    /* cout << "Pre-order tree: ";
+    preOder(root);
+    cout << endl; */
+
+    cout << "In-order tree: ";
+    inOrder(root);
+    cout << endl;
+
+    /* cout << "Post-order tree: ";
+    postOrder(root);
+    cout << endl; */
 
     return 0;
 }
