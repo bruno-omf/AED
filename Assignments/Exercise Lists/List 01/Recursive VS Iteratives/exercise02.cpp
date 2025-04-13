19bfb1e9587f28c640c58e69cb3f0b3acc29fc38
Bruno <bomf@cesar.school>
2025-04-13 10:47:55 -0300

    add 2nd exercise of recursive algorithms

--- Assignments/Exercise Lists/List 01/Recursive VS Iteratives/exercise02.cpp
+++ Assignments/Exercise Lists/List 01/Recursive VS Iteratives/exercise02.cpp
@@ -9,9 +9,42 @@
 
 using namespace std;
 
+int maiorElemento(int vetor[], int tamanho) {
+    // caso base
+    if (tamanho == 1) {
+        return vetor[0];
+    }
+    // caso recursivo
+    int maiorDoResto = maiorElemento(vetor, tamanho - 1);
 
+    if (vetor[tamanho - 1] > maiorDoResto) {
+        return vetor[tamanho - 1];
+    } else {
+        return maiorDoResto;
+    }
+}
 
 int main() {
+    int tamanho;
+    int maior;
+
+    cout << "Voce vai montar um vetor." << endl;
+    cout << "Informe o tamanho do vetor: ";
+    cin >> tamanho;
+
+    int vetor[tamanho];
+    cout << "Agora informe os elementos do vetor: " << endl;
+    for (int i = 0; i < tamanho; i++) {
+        cout << "Posicao " << i << ": ";
+        cin >> vetor[i];
+    }
+    
+    cout << "Vetor preenchido." << endl;
+
+    maior = maiorElemento(vetor, tamanho);
+    cout << "O maior valor armazenado no vetor eh: " << maior << endl;
 
     return 0;
 }
+
+// questao maneira, gostei muito dessa.
