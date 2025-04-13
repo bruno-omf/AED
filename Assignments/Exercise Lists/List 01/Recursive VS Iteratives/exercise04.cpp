bad9b0afb6a2bfd2884d0aaf849bee96eca064e3
Bruno <bomf@cesar.school>
2025-04-13 11:55:13 -0300

    add 4th exercise of recursive algorithms

--- Assignments/Exercise Lists/List 01/Recursive VS Iteratives/exercise04.cpp
+++ Assignments/Exercise Lists/List 01/Recursive VS Iteratives/exercise04.cpp
@@ -10,9 +10,25 @@
 
 using namespace std;
 
-
+int produto(int numero, int vezes) {
+    if (numero == 0 || vezes == 0) {
+        return 0;
+    } else {
+        return numero + produto(numero, vezes - 1);
+    }
+}
 
 int main() {
+    int numero, vezes, resultado;
 
+    cout << "Voce vai calcular o produto de dois numeros inteiros." << endl;
+    cout << "Digite o primeiro numero: ";
+    cin >> numero;
+
+    cout << "Digite quantas vezes quer multiplicar esse numero inserido: ";
+    cin >> vezes;
+
+    resultado = produto(numero, vezes);
+    cout << "O resultado da operacao : " << numero << "x" << vezes << " eh: " << resultado << endl;
     return 0;
 }
