0e9d61555ae2e9e7c2cf85862749037964789ec4
Bruno <bomf@cesar.school>
2025-04-14 14:52:28 -0300

    add 8th exercise of stack algorithm

--- Assignments/Exercise Lists/List 01/List, Stack and Queue/exercise08.cpp
+++ Assignments/Exercise Lists/List 01/List, Stack and Queue/exercise08.cpp
@@ -11,8 +11,56 @@
 
 using namespace std;
 
+#define MAX 32
 
+typedef struct Stack {
+    int data[MAX];
+    int top;
+} Stack;
+
+void initializeStack(Stack* stack) {
+    stack->top = -1;
+}
+
+void push(Stack* stack, int value) {
+    if (stack->top < MAX - 1) {
+        stack->data[++(stack->top)] = value;
+    }
+}
+
+int pop(Stack* stack) {
+    if (stack->top >= 0) {
+        return stack->data[(stack->top)--];
+    }
+    return -1;
+}
+
+void decimalToBinary(int number) {
+    Stack stack;
+    initializeStack(&stack);
+
+    if (number == 0) {
+        cout << "0" << endl;
+        return;
+    }
+
+    while (number > 0) {
+        push(&stack, number % 2);
+        number /= 2;
+    }
+
+    while (stack.top >= 0) {
+        cout << pop(&stack);
+    }
+    cout << endl;
+}
 
 int main() {
+    int number1 = 10;
+    int number2 = 25;
 
+    decimalToBinary(number1);
+    decimalToBinary(number2);
+
+    return 0;
 }
