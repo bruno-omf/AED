8a68a6330cfdeddf4bfa4b8c0be42a027e4f72b1
Bruno <bomf@cesar.school>
2025-04-14 10:50:34 -0300

    add 6th exercise of recusive algorithms

--- Assignments/Exercise Lists/List 01/Recursive VS Iteratives/exercise06.cpp
+++ Assignments/Exercise Lists/List 01/Recursive VS Iteratives/exercise06.cpp
@@ -7,12 +7,34 @@
 #include <stdio.h>
 #include <iostream>
 #include <stdlib.h>
+#include <string.h>
 
 using namespace std;
 
+bool palindromo(char palavra[], int inicio, int fim) {
+    if (inicio >= fim) { // caso base
+        return true;
+    }
 
+    if (palavra[inicio] != palavra[fim]) {
+        return false;
+    }
+
+    return palindromo(palavra, inicio + 1, fim - 1);
+}
 
 int main() {
+    char palavra[50];
+    bool resultado;
+
+    cout << "Voce vai visualizar se uma palavra eh palindromo ou nao." << endl;
+    cout << "Digite uma palavra: ";
+    cin >> palavra;
+
+    int tamanho = strlen(palavra);
+
+    resultado = palindromo(palavra, 0, tamanho - 1);
+    cout << "A palavra eh palindromo?" << endl << "-> " << resultado << endl;;
 
     return 0;
 }
