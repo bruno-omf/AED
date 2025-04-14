34e8462f2437abb0f83a62192b6ddbdc60b6c79a
Bruno <bomf@cesar.school>
2025-04-14 14:41:33 -0300

    add 4th exercise of list algorithms

--- Assignments/Exercise Lists/List 01/List, Stack and Queue/exercise04.cpp
+++ Assignments/Exercise Lists/List 01/List, Stack and Queue/exercise04.cpp
@@ -14,19 +14,52 @@ typedef struct Node {
 } Node;
 
 Node* createNode(int data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->next = NULL;
 
+    return newNode;
 }
 
 Node* insertAtEnd(Node* head, int data) {
+    Node* newNode = createNode(data);
+    newNode->data = data;
+    newNode->next = NULL;
 
+    if (head == NULL) {
+        return newNode;
+    }
+
+    Node* temp = head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+
+    return head;
 }
 
 Node* reverseList(Node*head) {
+    Node* prev = NULL;
+    Node* current = head;
+    Node* next = NULL;
 
+    while (current != NULL) { // enquanto a lista não estiver vazia
+        next = current->next; // armazeno o próximo nó
+        current->next = prev; // o nó atual aponta para o nó anterior
+        prev = current; // o nó anterior agora é o nó atual
+        current = next; // passo para o próximo nó
+    }
+    return prev; // o novo head é o último nó que foi processado
 }
 
 void printList(Node* head) {
-
+    Node* temp = head;
+    while (temp != NULL) {
+        cout << temp->data << " -> ";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
 }
 
 int main() {
