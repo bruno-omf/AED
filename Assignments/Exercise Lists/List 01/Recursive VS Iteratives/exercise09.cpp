c8460e89f823c30ddc163532555cf9f33b2cc126
Bruno <bomf@cesar.school>
2025-04-14 11:14:02 -0300

    add 9th exercise of iterative algorithms

--- Assignments/Exercise Lists/List 01/Recursive VS Iteratives/exercise09.cpp
+++ Assignments/Exercise Lists/List 01/Recursive VS Iteratives/exercise09.cpp
@@ -7,9 +7,26 @@
 
 using namespace std;
 
+int somaDigitos(int numero) {
+    int soma = 0;
 
+    while (numero > 0) {
+        soma = soma + numero % 10; // adiciona o digito da "unidade" do numero à soma
+        numero = numero / 10; // remove o ultimo digito do numero
+    }
+    return soma;
+}
 
 int main() {
+    int numero;
+    int soma;
+
+    cout << "Voce vai somar os digitos de um numero." << endl;
+    cout << "Digite um numero: ";
+    cin >> numero;
+
+    soma = somaDigitos(numero);
+    cout << "A soma dos digitos do numero " << numero << " eh: " << soma << endl;
 
     return 0;
 }
