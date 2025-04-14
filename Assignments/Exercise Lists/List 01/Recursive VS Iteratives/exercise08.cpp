fa07d23782c989c291e4d2172e531545271ad883
Bruno <bomf@cesar.school>
2025-04-14 11:09:37 -0300

    add 8th exercise of iterative algorithms

--- Assignments/Exercise Lists/List 01/Recursive VS Iteratives/exercise08.cpp
+++ Assignments/Exercise Lists/List 01/Recursive VS Iteratives/exercise08.cpp
@@ -7,9 +7,36 @@
 
 using namespace std;
 
+int maiorElemento(int vetor[], int tamanho) {
+    int maior = vetor[0];
 
+    for (int i = 0; i < tamanho; i++) {
+        if (vetor[i] > maior) {
+            maior = vetor[i];
+        }
+    }
+    return maior;
+}
 
 int main() {
+    int tamanho;
+    int maior;
+
+    cout << "Voce vai montar um vetor de inteiros." << endl;
+    cout << "Informe o tamanho do vetor: ";
+    cin >> tamanho;
+
+    int vetor[tamanho];
+    cout << "Agora informe os elementos do vetor: " << endl;
+    for (int i = 0; i < tamanho; i++) {
+        cout << "Posicao " << i << ": ";
+        cin >> vetor[i];
+    }
+    
+    cout << "Vetor preenchido." << endl;
+
+    maior = maiorElemento(vetor, tamanho);
+    cout << "O maior valor armazenado no vetor eh: " << maior << endl;
 
     return 0;
 }
