a815c904a43cfb6d895506466957c4b6383d3d90
Bruno <bomf@cesar.school>
2025-04-14 11:51:26 -0300

    add initial code exercise 01 list

--- Assignments/Exercise Lists/List 01/List, Stack and Queue/exercise01.cpp
+++ Assignments/Exercise Lists/List 01/List, Stack and Queue/exercise01.cpp
@@ -11,9 +11,40 @@
 #include <iostream>
 
 using namespace std;
+typedef struct Node {
+    int data;
+    struct Node* next;
+} Node;
 
+Node* insertAtStart(Node* root, int data) {
 
+}
+
+Node* insertAtEnd(Node* root, int data) {
+
+}
+
+Node* removeNode(Node* root, int data) {
+
+}
+
+void printList(Node* root) {
+
+}
 
 int main() {
+    Node* root = NULL;
+
+    root = insertAtStart(root, 10);
+    root = insertAtStart(root, 40);
+    root = insertAtStart(root, 70);
+    printList(root);
+
+    root = insertAtEnd(root, 20);
+    root = insertAtEnd(root, 50);
+    printList(root);
+
+    removeNode(root, 70);
+    printList(root);
 
 }
