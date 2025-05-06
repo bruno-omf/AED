f56ce2be99b51e51f7324cf5347c569485e0d159
Bruno <bomf@cesar.school>
2025-05-06 12:07:44 -0300

    add comments to binarySearch code

--- Classes/Class 6 - Searching Algorithms/binarySearch.cpp
+++ Classes/Class 6 - Searching Algorithms/binarySearch.cpp
@@ -9,14 +9,14 @@ int binarySearch(int vector[], int key) {
     int start = 0, end = SIZE - 1, middle;
 
     while (start <= end) {
-        middle = (start + end) / 2;
+        middle = (start + end) / 2; // divisoes sucessivas do array pra poder encontrar o meio dele, ou seja, o valor do meio do array
 
         if (vector[middle] == key) {
             return middle; // valor encontrado
-        } else if (vector[middle] < key) {
-            start = middle + 1;
-        } else {
-            end = middle - 1;
+        } else if (vector[middle] < key) { // se o valor atual da variavel meio for menor que o valor a ser buscado:
+            start = middle + 1; // tenho que buscar na parte direita do array, ou seja, o meio atual e os valores a esquerda dele sao descartados
+        } else { // se o valor atual da variavel meio for maior que o valor a ser buscado:
+            end = middle - 1; // tenho que buscar na parte esquerda do array, ou seja, o meio atual e os valores a direita dele sao descartados
         }
     }
     return -1; // nao encontrado
