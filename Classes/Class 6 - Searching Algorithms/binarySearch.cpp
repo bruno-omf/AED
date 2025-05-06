2c72b53690fb7cbbaebde36bf621a39a59dd645f
Bruno <bomf@cesar.school>
2025-05-06 12:00:43 -0300

    add binarySearch code

--- Classes/Class 6 - Searching Algorithms/binarySearch.cpp
+++ Classes/Class 6 - Searching Algorithms/binarySearch.cpp
@@ -1,7 +1,48 @@
 #include <iostream>
 # include <stdio.h>
 
+#define SIZE 12
+
+using namespace std;
+
+int binarySearch(int vector[], int key) {
+    int start = 0, end = SIZE - 1, middle;
+
+    while (start <= end) {
+        middle = (start + end) / 2;
+
+        if (vector[middle] == key) {
+            return middle; // valor encontrado
+        } else if (vector[middle] < key) {
+            start = middle + 1;
+        } else {
+            end = middle - 1;
+        }
+    }
+    return -1; // nao encontrado
+}   
+
 int main() {
+    int vec[] = {8, 10, 12, 15, 20, 30, 33, 34, 50, 55, 82, 95};
+    int key = 0;
+    int index;
+
+    cout << "Vector {";
+    for (int i = 0; i < SIZE; i++) {
+        cout << vec[i] << ",";
+    }
+    cout << "}" << endl;
+
+    cout << "Which numer do you want to search for?" << ": " << endl;
+    cin >> key;
+
+    index = binarySearch(vec, key);
+
+    if (index == -1) {
+        cout << "The number " << key << " was not found in the vector." << endl;
+    } else {
+        cout << "The number " << key << " was found in the vector at the position [" << index << "]" << endl;
+    }
     
     return 0;
 }
