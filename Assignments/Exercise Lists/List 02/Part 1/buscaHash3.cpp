f8aa38999a3f9ad57005d957118d7b28080c46a4
Bruno <bomf@cesar.school>
2025-05-22 19:25:02 -0300

    add buscaHash3.cpp full code

--- Assignments/Exercise Lists/List 02/Part 1/buscaHash3.cpp
+++ Assignments/Exercise Lists/List 02/Part 1/buscaHash3.cpp
@@ -20,15 +20,25 @@ int funcaoHash(int chave) {
     return chave % TAM;
 }
 
-int inserir() {
+int inserir(int chave) {
+    int indice = funcaoHash(chave);
 
+    for (int i = 0; i < TAM; i++) {
+        int tentativa = (indice + i) % TAM;
+
+        if (tabelaHash[tentativa] == VAZIO || tabelaHash[tentativa] == REMOVIDO) {
+            tabelaHash[tentativa] = chave;
+            return tentativa; // inserido com sucesso!
+        }
+    }
+    return -1; // tabela cheia
 }
 
 int buscar(int chave) { // busca com sondagem linear
     int indice = funcaoHash(chave); // indice 7 (passando 37 como chave)
 
     for (int i = 0; i < TAM; i++) { // vai procurando do indice 7 em diante se o valor foi inserido.
-        int tentativa = (indice + i) % TAM;
+        int tentativa = (indice + i) % TAM; // se passar do indice 9, a tentativa é = 10. quando a tentativa é = 10, o modulo é 0, logo, o elemento vai ser adicionado no indice 0 da tabela hash. MUITO MASSA!
 
         if (tabelaHash[tentativa] == VAZIO) {
             return -1; // nao encontrado (nunca foi inserido)
@@ -47,7 +57,10 @@ int remover() {
 }
 
 void imprimeTabela() {
-
+    cout << "Tabela Hash: " << endl;
+    for (int i = 0; i < TAM; i++) {
+        cout << tabelaHash[i] << endl;
+    }
 }
 
 int main() {
@@ -65,14 +78,14 @@ int main() {
     }
 
     // depois inserir os numeros 17, 27, 37, 47, 57
-    // inserir(17);
-    // inserir(27);
-    // inserir(37);
-    // inserir(47);
-    // inserir(57);
+    inserir(17);
+    inserir(27);
+    inserir(37);
+    inserir(47);
+    inserir(57);
 
     // // e por fim imprimir a tabela
-    // imprimeTabela();
+    imprimeTabela();
 
 
     return 0;
