7e19f6c6381bca1ee21d3e5f304dcd3e0de90167
Bruno <bomf@cesar.school>
2025-05-22 18:55:39 -0300

    add buscaHash3.cpp main code

--- Assignments/Exercise Lists/List 02/Part 1/buscaHash3.cpp
+++ Assignments/Exercise Lists/List 02/Part 1/buscaHash3.cpp
@@ -33,6 +33,29 @@ void imprimeTabela() {
 }
 
 int main() {
+    // primeiro inicializar a tabela hash
+    inicializaTabela();
+
+    // depois buscar o numero 37 na tabela (que obviamente nao vai ter)
+    int busca = 37;
+    int posicao = buscar(busca);
+
+    if (posicao != -1) {
+        cout << "Elemento " << busca << " encontrado na posicao: " << posicao << endl;
+    } else {
+        cout << "Elemento " << busca << " nao encontrado." << endl;
+    }
+
+    // depois inserir os numeros 17, 27, 37, 47, 57
+    inserir(17);
+    inserir(27);
+    inserir(37);
+    inserir(47);
+    inserir(57);
+
+    // e por fim imprimir a tabela
+    imprimeTabela();
+
 
     return 0;
 }
