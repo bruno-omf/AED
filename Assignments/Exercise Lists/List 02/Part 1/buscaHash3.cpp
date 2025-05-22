1769b4b6a5f6b2eec1f67da6e6bd3fee28071ce3
Bruno <bomf@cesar.school>
2025-05-22 18:28:17 -0300

    rename buscaHash.cpp and add buscaHash2.cpp and buscaHash3.cpp

---
+++ Assignments/Exercise Lists/List 02/Part 1/buscaHash3.cpp
@@ -0,0 +1,38 @@
+// 9. Hash com sondagem linear, buscar 37: Inserir 17, 27, 37, 47, 57
+
+#include <iostream>
+
+#define TAM 10
+#define VAZIO -1
+#define REMOVIDO -2
+
+int tabelaHash[TAM];
+
+void inicializaTabela() {
+
+}
+
+int funcaoHash() {
+
+}
+
+int inserir() {
+
+}
+
+int buscar() {
+
+}
+
+int remover() {
+
+}
+
+void imprimeTabela() {
+
+}
+
+int main() {
+
+    return 0;
+}
