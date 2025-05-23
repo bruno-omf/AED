efd7b2fb9ca9fab1145f831526a0097fd0ae9a81
Bruno <bomf@cesar.school>
2025-05-23 11:46:17 -0300

    add insertioSort.cpp full code

--- Assignments/Exercise Lists/List 02/Part 2/insertionSort.cpp
+++ Assignments/Exercise Lists/List 02/Part 2/insertionSort.cpp
@@ -10,34 +10,75 @@
 using namespace std;
 
 void insertionSort(int vetor[], int tamanho) {
-
+    for (int i = 1; i < tamanho; i ++) {
+        int chave = vetor[i];
+        int j = i - 1;
+        
+        while (j >= 0 && vetor[j] > chave) {
+            vetor[j + 1] = vetor[j];
+            j--; // decrementa o J pra sair do loop
+        }
+        vetor[j + 1] = chave;
+    }
 }
 
-void insertionSortEven(int vetor[], int tamanho) {
+void sortEvenOnly(int vetor[], int tamanho) {
+    // Passo 1: extrair pares
+    int pares[TAM];
+    int countPares = 0;
+    for (int i = 0; i < tamanho; i++) {
+        if (vetor[i] % 2 == 0) {
+            pares[countPares++] = vetor[i];
+        }
+    }
 
+    // Passo 2: ordenar vetor de pares
+    insertionSort(pares, countPares);
+
+    // Passo 3: substituir pares ordenados no vetor original
+    int indexPar = 0;
+    for (int i = 0; i < tamanho; i++) {
+        if (vetor[i] % 2 == 0) {
+            vetor[i] = pares[indexPar++];
+        }
+    }
+}
+
+void printaSortEvenOnly(int vetor[], int tamanho) {
+    for (int i = 0; i < tamanho; i++) {
+        cout << vetor[i] << " ";
+    }
+    cout << endl;
 }
 
 void printCrescente(int vetor[], int tamanho) {
-
+    for (int i = 0; i < tamanho; i++) {
+        cout << vetor[i] << " ";
+    }
+    cout << endl;
 }
 
 void printDecrescente(int vetor[], int tamanho) {
-
+    for (int i = tamanho - 1; i >=0 ; i--) {
+        cout << vetor[i] << " ";
+    }
+    cout << endl;
 }
 
 int main () {
     int vet1[TAM] = {22, 11, 99, 88, 9, 7, 42, 65, 33, 10};
     int vet2[TAM] = {9, 8, 3, 4, 7, 6, 1, 2, 5, 0};
     int vet3[TAM] = {30, 20, 10, 40, 50, 70, 60, 90, 80, 100};
+
     // ordenar o 1° vetor e printar
     insertionSort(vet1, TAM);
     cout << "Vetor 1 ordenado: ";
     printCrescente(vet1, TAM);
 
     // ordenar apenas valores pares no 2° vetor e printar
-    insertionSortEven(vet2, TAM);
+    sortEvenOnly(vet2, TAM);
     cout << "Vetor 2, ordenando apenas os valores pares: ";
-    printCrescente(vet2, TAM);
+    printaSortEvenOnly(vet2, TAM);
 
     // ordenar o 3° vetor e printar em ordem decrescente
     insertionSort(vet3, TAM);
